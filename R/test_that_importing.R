8fe33690fc939373de7d349885618d88230d47d6
Paolo De Lagrave-Codina <paolo.delagrave@gmail.com>
2023-11-25 13:54:29 -0800

    update functions

--- R/test_that_importing.R
+++ R/test_that_importing.R
@@ -1,28 +1,5 @@
-# The sample from demo to take place
 library(testthat)
-source("../../R/count_classes.R")
-
-# Assuming your function to import data is called 'importing'
-# And it takes a file path as an argument
-#source("path_to_your_function/importing.R") # adjust the path to your actual function
-
-
-
-
-#' Read in Data
-#'
-#' Reads data from a CSV file into a data frame.
-#'
-#' @param file_path The path to the CSV file containing data.
-#'
-#' @return A data frame.
-#'
-#' @examples
-#' egg_prod <- read_egg_production_data("../data/egg-production.csv")
-#' egg_prod
-importing <- function(file_path){
-  readr::read_csv(file_path)
-}
+source("../R/function_importing.R")
 
 test_that("importing() returns a data frame", {
   file_path <- "../data/egg-production.csv"
@@ -33,14 +10,14 @@ test_that("importing() returns a data frame", {
   expect_true(is.data.frame(imported_data))
 })
 
-# Here's how you would test if the imported data has the right column names
+
 test_that("importing() returns data frame with correct column names", {
-  file_path <- "../data/egg-production.csv" # adjust this to the path of your actual file
-  expected_col_names <- c("observed_month", "prod_type", "prod_process", "n_hens", "n_eggs", "source") # replace with your actual column names
-  
+  file_path <- "../data/egg-production.csv" # adjust this to the path
+  expected_col_names <- c("observed_month", "prod_type", "prod_process", "n_hens", "n_eggs", "source")
+
   imported_data <- importing(file_path)
   actual_col_names <- names(imported_data)
-  
+
   expect_equal(actual_col_names, expected_col_names)
 })
 
